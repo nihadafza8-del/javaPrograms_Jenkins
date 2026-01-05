83497b18640e69e3973d507b8823bc1ae1a02e73
nihad <nihad.afza123@gmail.com>
2026-01-05 15:24:35 +0530

    Added Student class

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating a String object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi, I am student");
+    }
+}
