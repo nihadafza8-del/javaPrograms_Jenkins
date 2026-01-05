d92e2682e18a9eb7a6bf515b596cfb68580fc8df
nihad <nihad.afza123@gmail.com>
2026-01-05 15:03:32 +0530

    Update Main.java
    First Commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("This is Java Program");
         System.out.println("Output from Java Program");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
